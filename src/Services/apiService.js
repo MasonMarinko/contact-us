bf38c29ac9b8460ae3f76c96d06570e553d49cc2
MasonM <Mason.P.Marinko@gmail.com>
2021-06-08 20:53:00 -0600

    updated build

--- src/Services/apiService.js
+++ src/Services/apiService.js
@@ -1,16 +1,7 @@
 const axios = require('axios');
 
-let base
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  // dev code
-  base = "https://my-json-server.typicode.com/JustUtahCoders/interview-users-api"
-} else {
-  // production code
-  base = "https://www.contactform.com"
-}
-
 const instance = axios.create({
-    baseURL: base
+    baseURL: "https://my-json-server.typicode.com/JustUtahCoders/interview-users-api"
   });
 
   module.exports = instance
