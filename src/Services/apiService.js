d4b34eeadff1d3ba50d30ca2dc6d90971127b328
MasonM <Mason.P.Marinko@gmail.com>
2021-06-08 20:48:34 -0600

    updated apiService just to post, if statement not needed

--- src/Services/apiService.js
+++ src/Services/apiService.js
@@ -3,14 +3,14 @@ const axios = require('axios');
 let base
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   // dev code
-  base = "http://localhost:4500"
+  base = "https://my-json-server.typicode.com/JustUtahCoders/interview-users-api"
 } else {
   // production code
   base = "https://www.contactform.com"
 }
 
 const instance = axios.create({
-    baseURL: base + '/api'
+    baseURL: base
   });
 
   module.exports = instance
